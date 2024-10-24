65646d52a14234500a63f003006ef29112313b72
trinh7293 <trinh7293@gmail.com>
2024-10-24 16:07:35 +0700

    add test customNode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+// import App from './App'
+import Test1 from './test1/Test1'
 
-import './index.css'
+// import './index.css'
+import './test1/test1.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Test1 />
   </React.StrictMode>
 )
